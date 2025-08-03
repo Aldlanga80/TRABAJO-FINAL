f2ee3b08e3882939bbf12e47f263beab134aae05
Aldlanga80 <aldo.d.langa@gmail.com>
2025-08-03 17:55:39 -0300

    Inicio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="error">
+      <p>Sitio desarrollado por <a href="https://www.linkedin.com/in/gabriel-alberini/" target="_blank">Gabriel Alberini</a></p>
+    </footer>
+  )
+}
+
+export { Footer }
