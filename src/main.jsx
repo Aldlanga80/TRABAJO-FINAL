22a88bc61691bdd30d25ef3ac019db595fc6ff77
Aldlanga80 <aldo.d.langa@gmail.com>
2025-08-05 16:09:00 -0300

    Import de los archivos css en main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterApp } from './router/RouterApp'
 
+
+import './styles/components/Footer.css'
 import './styles/components/Header.css'
 import './styles/pages/Home.css'
+import './styles/pages/Login.css'
+import './styles/pages/Register.css'
+import './styles/pages/Dashboard.css'
+import './styles/pages/NotFound.css'
 import './index.css';
+
 import { UserProvider } from './context/UserContext'
 
 createRoot(document.getElementById('root')).render(
